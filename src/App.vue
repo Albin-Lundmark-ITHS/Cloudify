142e2932e982699102c300853bccf4ec091e6389
swedush <122605721+swedush@users.noreply.github.com>
2024-02-21 12:13:27 +0100

    Removed generic css and added link to footer.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import FooterBar from './components/FooterBar.vue'
 </script>
 
 <template>
   <header>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
   </header>
   <RouterView />
+  <FooterBar />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
