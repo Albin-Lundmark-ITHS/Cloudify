4e9e0983e27b1d8c278859adbe2efdadfe2c005c
swedush <122605721+swedush@users.noreply.github.com>
2024-02-22 16:07:53 +0100

    added text

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -2,40 +2,72 @@
 
 <template>
   <footer class="footer">
-    <div id="iconsFooter">
-      <div id="footerIcon">Icon1</div>
-      <div id="footerIcon">Icon2</div>
-      <div id="footerIcon">Icon3</div>
-    </div>
     <div id="linksFooter">
-      <a href="#">Link1</a>
-      <a href="#">Link2</a>
-      <a href="#">Link3</a>
+      <RouterLink to="/">TEXT</RouterLink>
+      <RouterLink to="/">TEXT</RouterLink>
+      <RouterLink to="/about">TEXT</RouterLink>
+    </div>
+    <div id="iconsFooter">
+      <a href="https://www.facebook.com/" target="_blank">
+        <v-icon class="footerIcon" icon="mdi-facebook"></v-icon>
+      </a>
+      <a href="https://www.github.com/" target="_blank">
+        <v-icon class="footerIcon" icon="mdi-github"></v-icon>
+      </a>
+      <a href="https://www.linkedin.com/" target="_blank">
+        <v-icon class="footerIcon" icon="mdi-linkedin"></v-icon>
+      </a>
+      <!--  <a href="https://www.youtube.com/" target="_blank">
+        <v-icon class="footerIcon" icon="mdi-youtube"></v-icon>
+      </a> -->
+      <a href="https://www.instagram.com/" target="_blank">
+        <v-icon class="footerIcon" icon="mdi-instagram"></v-icon>
+      </a>
+      <!--    <a href="https://www.twitter.com/" target="_blank">
+        <v-icon class="footerIcon" icon="mdi-twitter"></v-icon>
+      </a> -->
     </div>
   </footer>
 </template>
 
 <style scoped>
-#footerIcon {
-  border: solid black 4px;
-  margin: 5px;
+#footerLogo {
+  height: auto;
+  width: 50px;
+}
+.footerIcon {
+  width: 32px;
+  height: 32px;
+  font-size: 32px;
 }
 #iconsFooter {
   color: black;
+  margin: 20px;
   display: flex;
   justify-content: center;
+  gap: 55px;
 }
 #linksFooter {
+  margin-top: 10px;
+  gap: 40px;
   display: flex;
   justify-content: center;
 }
 .footer {
   position: absolute;
   bottom: 0;
+  height: 100px;
   width: 100%;
   background-color: #d4f0fd;
   color: white;
   text-align: center;
+  box-shadow: 10px 5px 20px rgb(0, 0, 0);
+}
+a:visited {
+  color: black;
+}
+a {
+  text-decoration: none;
 }
 </style>
 
