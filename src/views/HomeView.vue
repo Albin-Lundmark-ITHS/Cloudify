65c08dd444987f66847164166a03ab75dbddfc04
Malin Johansson <Malinjohansson441@hotmail.com>
2024-02-26 09:47:10 +0100

    Testa API i väderkortet

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
-<script setup></script>
+<script setup>
+import WeatherCards from '../components/WeatherCards.vue'</script>
 
 <template>
   <main>
     <h1>Home</h1>
+    <WeatherCards />
   </main>
 </template>
