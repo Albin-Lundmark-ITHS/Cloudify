632bd77acad42047c4c7061affc83380fbc86b2e
isabellea-f <isabelle.amsalu@gmail.com>
2024-02-28 11:34:26 +0100

    created form

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -45,6 +45,69 @@
         <h3>Address:</h3>
         <h4>Cloud St 15, New York</h4>
       </div>
+      <div id="form-container">
+        <h2>Title</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur blanditiis non esse
+          necessitatibus ratione labore repellendus beatae veniam? Consequuntur beatae laboriosam,
+          sed, amet rerum necessitatibus distinctio ipsam pariatur facilis, nihil culpa natus
+          reiciendis? Amet unde sunt alias reprehenderit nihil quidem repellat expedita, ab maxime,
+          saepe dignissimos, adipisci vitae quo error cupiditate? Velit maiores minima dolore
+          mollitia, eos odit esse quam aut facilis veritatis delectus reiciendis aperiam quibusdam
+          suscipit asperiores possimus nemo nesciunt saepe unde. Ut, vitae. Nihil corporis error
+          dicta rerum distinctio? Saepe suscipit veritatis nemo, unde excepturi quae aliquid hic
+          illo deleniti sit obcaecati, laudantium quisquam, sed praesentium dolorem?
+        </p>
+
+        <div class="container">
+          <form action="/action_page.php">
+            <div class="row">
+              <div class="col-25">
+                <label for="fname">First Name</label>
+              </div>
+              <div class="col-75">
+                <input type="text" id="fname" name="firstname" placeholder="Your name.." />
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-25">
+                <label for="lname">Last Name</label>
+              </div>
+              <div class="col-75">
+                <input type="text" id="lname" name="lastname" placeholder="Your last name.." />
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-25">
+                <label for="country">Country</label>
+              </div>
+              <div class="col-75">
+                <select id="country" name="country">
+                  <option value="sweden">Sweden</option>
+                  <option value="norway">Norway</option>
+                  <option value="denmark">Denmark</option>
+                </select>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-25">
+                <label for="subject">Subject</label>
+              </div>
+              <div class="col-75">
+                <textarea
+                  id="subject"
+                  name="subject"
+                  placeholder="Enter message here..."
+                  style="height: 200px"
+                ></textarea>
+              </div>
+            </div>
+            <div class="row">
+              <input type="submit" value="Submit" />
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -102,6 +165,74 @@
 
 #cloudifyLogo {
   height: 60px;
+}
+
+/* Form */
+
+input[type='text'],
+select,
+textarea {
+  width: 75%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  resize: vertical;
+  background-color: white;
+}
+
+label {
+  padding: 12px 12px 12px 0;
+  display: inline-block;
+}
+
+input[type='submit'] {
+  background-color: teal;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  margin-top: 20px;
+  float: right;
+}
+
+input[type='submit']:hover {
+  background-color: rgb(48, 95, 95);
+}
+/* 
+.container {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+} */
+
+.col-25 {
+  float: left;
+  width: 25%;
+  margin-top: 6px;
+}
+
+.col-75 {
+  float: left;
+  width: 75%;
+  margin-top: 6px;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: '';
+  display: table;
+  clear: both;
+}
+
+/* Responsive layout - when the screen is less than 600px wide, make the two columns stack on top of each other instead of next to each other */
+@media screen and (max-width: 600px) {
+  .col-25,
+  .col-75,
+  input[type='submit'] {
+    width: 100%;
+    margin-top: 0;
+  }
 }
 
 @media only screen and (max-width: 1400px) {
