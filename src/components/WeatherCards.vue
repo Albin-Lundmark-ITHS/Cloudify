63a3e8cf5c34b76337677a7db98a9077d0702fcb
Hanna Klang Eriksson <hanna.klang.eriksson@gmail.com>
2024-03-06 16:21:40 +0100

    Created new weather store with fetch for weatherlist, weathercard and searchpanel

--- src/components/WeatherCards.vue
+++ src/components/WeatherCards.vue
@@ -1,62 +1,61 @@
 <template>
-  <v-responsive class="mx-auto text-container">
-    <v-text-field
-      id="city-input"
-      label="Enter a city or country"
-      v-model="cityInput"
-      append-inner-icon="mdi-magnify"
-      class="text-field-input"
-      @keydown.enter="fetchWeatherData()"
-    ></v-text-field>
-  </v-responsive>
   <v-card :class="backgroundImage" class="weather-card mx-auto">
-    <v-card-item :title="currentCity + ', ' + currentCountry">
+    <v-card-item v-if="currentWeatherData">
       <template v-slot:subtitle>
-        {{ text }}
+        <!--  <v-icon icon="mdi-alert" size="18" color="error" class="me-1 pb-1"></v-icon> -->
+        {{ currentWeatherData.current.condition.text }}
+      </template>
+      <template v-slot:title>
+        {{ currentWeatherData.location.name }}
+        {{ currentWeatherData.location.country }}
       </template>
     </v-card-item>
 
-    <v-card-text class="py-0">
+    <v-card-text class="py-0" v-if="currentWeatherData && currentWeatherData.current">
       <v-row align:center no-gutters>
-        <v-col class="text-h2" cols="6"> {{ currentTemp }} </v-col>
+        <v-col class="text-h2" cols="6"> {{ currentWeatherData.current.temp_c }}&deg; </v-col>
 
         <v-col cols="6" class="text-right">
           <img
-            :src="currentConditionIcon"
-            alt="Current Weather"
-            style="width: 100px; height: 100px"
+            v-if="currentWeatherData.current.condition && currentWeatherData.current.condition.icon"
+            :src="currentWeatherData.current.condition.icon"
+            :alt="currentWeatherData.current.condition.text"
           />
         </v-col>
       </v-row>
     </v-card-text>
-    <v-card-text class="text-left last-updated"> Last updated at: {{ lastUpdated }} </v-card-text>
-    <!--    <div class="d-flex py-3 justify-space-between"></div> -->
+
+    <v-card-text class="text-left last-updated" v-if="currentWeatherData">
+      Last updated at: {{ currentWeatherData.current.last_updated }}
+    </v-card-text>
 
     <v-expand-transition>
       <div v-if="expand">
         <!-- Windspeed -->
         <v-list-item class="icon" density="compact" prepend-icon="mdi-weather-windy">
           <v-list-item-subtitle class="reportInfo"
-            >Wind Speed: {{ windSpeed }}</v-list-item-subtitle
+            >Wind Speed: {{ currentWeatherData.current.wind_kph }}</v-list-item-subtitle
           >
         </v-list-item>
 
         <!-- Humidity -->
         <v-list-item class="icon" density="compact" prepend-icon="mdi-water-percent">
-          <v-list-item-subtitle class="reportInfo">Humidity: {{ humidity }}</v-list-item-subtitle>
+          <v-list-item-subtitle class="reportInfo"
+            >Humidity: {{ currentWeatherData.current.humidity }}</v-list-item-subtitle
+          >
         </v-list-item>
         <!-- Feels like -->
 
         <v-list-item class="icon" density="compact" prepend-icon="mdi-thermometer-low">
           <v-list-item-subtitle class="reportInfo"
-            >Feels Like: {{ feelsLike }}</v-list-item-subtitle
+            >Feels Like: {{ currentWeatherData.current.feelslike_c }}</v-list-item-subtitle
           >
         </v-list-item>
         <!-- UV index -->
 
         <v-list-item class="icon" density="compact" prepend-icon="mdi-white-balance-sunny">
           <v-list-item-subtitle class="reportInfo">
-            UV Index: {{ uvIndex }}</v-list-item-subtitle
+            UV Index: {{ currentWeatherData.current.uv }}</v-list-item-subtitle
           ></v-list-item
         >
       </div>
@@ -73,131 +72,106 @@
 </template>
 
 <script>
-import axios from 'axios'
+import { useWeatherStore } from '@/store/WeatherStore'
+import { ref, onMounted, watch } from 'vue'
 
 export default {
-  data: () => ({
-    expand: false,
-    lastUpdated: '',
-    currentTemp: '',
-    currentConditionIcon: '',
-    windSpeed: '',
-    humidity: '',
-    text: '',
-    currentCity: '',
-    currentCountry: '',
-    cityInput: 'Stockholm',
-    weatherCode: 0,
-    isDay: 0
-  }),
-  watch: {
-    cityInput() {
-      this.fetchWeatherData()
-    }
-  },
-  methods: {
-    fetchWeatherData() {
-      const url = `http://api.weatherapi.com/v1/current.json?key=3cdf684a5804447ebbb83645240503&q=${this.cityInput}&aqi=no`
+  setup() {
+    const WeatherStore = useWeatherStore()
+    const currentWeatherData = ref(null)
+    const weatherCode = ref(null)
+   const isDay = ref(null)
 
-      axios
-        .get(url)
-        .then((response) => {
-          const data = response.data
-          this.currentTemp = `${data.current.temp_c}°C`
-          this.currentConditionIcon = data.current.condition.icon
-          this.windSpeed = `${data.current.wind_kph} km/h`
-          this.humidity = `${data.current.humidity}%`
-          this.text = `${data.current.condition.text}`
-          this.currentCity = `${data.location.name}`
-          this.currentCountry = `${data.location.country}`
-          this.feelsLike = `${data.current.feelslike_c}°C`
-          this.uvIndex = `${data.current.uv}`
-          this.lastUpdated = `${data.current.last_updated}`
-          this.weatherCode = data.current.condition.code
-          this.isDay = data.current.is_day
-        })
-        .catch((error) => {
-          console.error('Could not fetch weather data:', error)
-        })
-    }
-  },
+    // Use onMounted to fetch data when the component is mounted.
+    onMounted(async () => {
+      await WeatherStore.getAutomaticPosition()
+      WeatherStore.getCurrentWeather(WeatherStore.weatherData.location.name)
+    })
 
-  created() {
-    this.fetchWeatherData()
+    // Watch for changes in CurrentWeatherStore.currentWeatherData.
+    watch(
+      () => WeatherStore.currentWeatherData,
+      (newData) => {
+        currentWeatherData.value = newData
+        weatherCode.value = newData.current.condition.code
+        isDay.value = newData.current.is_day
+      }
+    )
+
+    return { currentWeatherData, weatherCode, isDay }
   },
   computed: {
     backgroundImage() {
-      if (this.weatherCode === 1000 && this.isDay === 1) {
+      const code = this.weatherCode
+      const isDay = this.isDay
+
+      if (code === 1000 && isDay === 1) {
         return 'bg-sunny'
-      } else if (this.weatherCode === 1000 && this.isDay === 0) {
+      } else if (code === 1000 && isDay === 0) {
         return 'bg-clear-night'
-      } else if (
-        this.weatherCode === 1003 ||
-        this.weatherCode === 1006 ||
-        this.weatherCode === 1009
-      ) {
+      } else if (code === 1003 || code === 1006 || code === 1009) {
         return 'bg-cloudy'
-      } else if (
-        this.weatherCode === 1030 ||
-        this.weatherCode === 1135 ||
-        this.weatherCode === 1147
-      ) {
+      } else if (code === 1030 || code === 1135 || code === 1147) {
         return 'bg-foggy'
       } else if (
-        this.weatherCode === 1063 ||
-        this.weatherCode === 1072 ||
-        this.weatherCode === 1150 ||
-        this.weatherCode === 1153 ||
-        this.weatherCode === 1168 ||
-        this.weatherCode === 1180 ||
-        this.weatherCode === 1183 ||
-        this.weatherCode === 1186 ||
-        this.weatherCode === 1189 ||
-        this.weatherCode === 1192 ||
-        this.weatherCode === 1195 ||
-        this.weatherCode === 1198 ||
-        this.weatherCode === 1201 ||
-        this.weatherCode === 1240 ||
-        this.weatherCode === 1243 ||
-        this.weatherCode === 1246
+        code === 1063 ||
+        code === 1072 ||
+        code === 1150 ||
+        code === 1153 ||
+        code === 1168 ||
+        code === 1180 ||
+        code === 1183 ||
+        code === 1186 ||
+        code === 1189 ||
+        code === 1192 ||
+        code === 1195 ||
+        code === 1198 ||
+        code === 1201 ||
+        code === 1240 ||
+        code === 1243 ||
+        code === 1246
       ) {
         return 'bg-rain'
       } else if (
-        this.weatherCode === 1066 ||
-        this.weatherCode === 1069 ||
-        this.weatherCode === 1114 ||
-        this.weatherCode === 1117 ||
-        this.weatherCode === 1171 ||
-        this.weatherCode === 1204 ||
-        this.weatherCode === 1207 ||
-        this.weatherCode === 1210 ||
-        this.weatherCode === 1213 ||
-        this.weatherCode === 1216 ||
-        this.weatherCode === 1219 ||
-        this.weatherCode === 1222 ||
-        this.weatherCode === 1225 ||
-        this.weatherCode === 1237 ||
-        this.weatherCode === 1249 ||
-        this.weatherCode === 1252 ||
-        this.weatherCode === 1255 ||
-        this.weatherCode === 1258 ||
-        this.weatherCode === 1261 ||
-        this.weatherCode === 1264
+        code === 1066 ||
+        code === 1069 ||
+        code === 1114 ||
+        code === 1117 ||
+        code === 1171 ||
+        code === 1204 ||
+        code === 1207 ||
+        code === 1210 ||
+        code === 1213 ||
+        code === 1216 ||
+        code === 1219 ||
+        code === 1222 ||
+        code === 1225 ||
+        code === 1237 ||
+        code === 1249 ||
+        code === 1252 ||
+        code === 1255 ||
+        code === 1258 ||
+        code === 1261 ||
+        code === 1264
       ) {
         return 'bg-snowfall'
       } else if (
-        this.weatherCode === 1087 ||
-        this.weatherCode === 1273 ||
-        this.weatherCode === 1276 ||
-        this.weatherCode === 1279 ||
-        this.weatherCode === 1282
+        code === 1087 ||
+        code === 1273 ||
+        code === 1276 ||
+        code === 1279 ||
+        code === 1282
       ) {
         return 'bg-thunder'
       } else {
         return 'bg-default'
       }
     }
-  }
+  },
+  data: () => ({
+    expand: false,
+    time: 0
+  })
 }
 </script>
 
