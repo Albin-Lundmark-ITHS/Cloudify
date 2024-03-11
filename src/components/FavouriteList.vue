beda41615333ce5842d3dd211ac7b250fca808a7
Albin <abbelundmark@gmail.com>
2024-03-11 13:37:53 +0100

    Fixed functionality for adding and removing favourites, now shows correct after reloading page.

--- src/components/FavouriteList.vue
+++ src/components/FavouriteList.vue
@@ -10,10 +10,7 @@ export default {
       console.log(FavouriteStore.favourites)
     })
     return { FavouriteStore }
-  },
-  data: () => ({
-    items: []
-  })
+  }
   /* methods: {
     selectAlternative(item) {
       this.selected = [item]
@@ -28,16 +25,22 @@ export default {
 </script>
 
 <template>
-  <v-card class="favourite mx-auto" max-width="50%">
+  <v-card class="favourite mx-auto">
     <v-card-title v-if="FavouriteStore.favouriteCount > 0" class="text-center"
       >Your favourite locations</v-card-title
     >
     <v-card-title v-else class="text-center">Your favourites will be displayed here</v-card-title>
 
     <v-list v-for="favourite in FavouriteStore.favourites" :key="favourite">
-      <v-list-item prepend-icon="mdi-map-marker" append-icon="mdi-heart">{{
-        favourite
-      }}</v-list-item>
+      <v-list-item prepend-icon="mdi-map-marker" append-icon="mdi-heart"
+        >{{ favourite }}
+        <v-icon
+          class="float-right"
+          icon="mdi-delete"
+          @click="FavouriteStore.removeFromFavourites(favourite)"
+        >
+        </v-icon
+      ></v-list-item>
     </v-list>
   </v-card>
 </template>
@@ -45,5 +48,11 @@ export default {
 <style scoped>
 .favourite {
   margin-bottom: 145px;
+  max-width: 45%;
+}
+@media screen and (max-width: 640px) {
+  .favourite {
+    max-width: 95%;
+  }
 }
 </style>
