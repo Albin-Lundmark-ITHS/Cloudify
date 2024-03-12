4028ada70d5b411b274b406f0486185ba107edc9
Hanna Klang Eriksson <hanna.klang.eriksson@gmail.com>
2024-03-12 08:59:32 +0100

    Changed styling for button to open overlay

--- src/components/DailyForecastWeatherList.vue
+++ src/components/DailyForecastWeatherList.vue
@@ -8,6 +8,8 @@ export default {
     const currentWeatherData = ref(null)
     const selectedDay = ref(null)
 
+    const hoveredButtonIndex = ref(null)
+
     // Fetching data when the component is mounted.
     onMounted(async () => {
       await WeatherStore.getAutomaticPosition()
@@ -22,31 +24,40 @@ export default {
       }
     )
 
-    return { WeatherStore, currentWeatherData, selectedDay }
+    return { WeatherStore, currentWeatherData, selectedDay, hoveredButtonIndex }
   },
 
   data: () => ({
     expand: false,
     overlay: false,
-    isPointerDown: false
+    btnVariant: 'plain'
   }),
   methods: {
     openOverlay(day) {
       // Set the selected day and open the overlay.
       this.selectedDay = day
       this.overlay = true // Update the overlay state.
-      this.isPointerDown = true
+      /* this.isPointerDown = true */
     },
     closeOverlay() {
       // Close the overlay.
       this.selectedDay = null
       this.overlay = false // Update the overlay state.
-      this.isPointerDown = false
+      /* this.isPointerDown = false */
     },
     extractTime(dateTimeString) {
       // Split the string with date and time into an array, retrieves the third element of the array, the time part.
       const timePart = dateTimeString.split(' ')[2]
       return timePart || null
+    },
+    handlePointer(index) {
+      this.btnVariant = 'flat'
+      this.hoveredButtonIndex = index
+    },
+    handlePointerLeave() {
+      /* this.btnVariant = 'plain'
+      this.btnColor = 'black' */
+      this.hoveredButtonIndex = null
     }
   }
 }
@@ -80,9 +91,12 @@ export default {
         </v-card-text>
         <v-card-text class="text-content"> {{ Math.round(day.day.maxtemp_c) }}&deg;C </v-card-text>
         <v-btn
-          variant="plain"
+          :variant="btnVariant"
+          :color="index === hoveredButtonIndex ? '#2C4E74' : ''"
           size="small"
-          @pointerdown="openOverlay(day)"
+          @pointerover="handlePointer(index)"
+          @pointerout="handlePointerLeave"
+          @click="openOverlay(day)"
           icon="mdi-chevron-right"
         ></v-btn>
         <v-divider class="border-opacity-100"></v-divider>
@@ -186,19 +200,20 @@ export default {
   margin-top: 0.1vh;
 }
 
-/* .v-btn {
+.v-btn {
   background-color: white;
   color: black;
 }
- .v-btn:hover {
+
+.v-btn:active {
   background-color: #2c4e74;
   color: white;
-} */
+}
 
-.button-down {
+/* .button-down {
   background-color: blue;
   color: white;
-}
+} */
 
 @media (max-width: 600px) {
   .mx-auto {
@@ -233,11 +248,13 @@ export default {
     color: black;
   }
 
-  .v-btn:hover {
+  */
+
+  /*  .v-btn:hover {
     background-color: #2c4e74;
     color: white;
-  } */
-
+  }
+ */
   .overlay {
     width: auto;
     height: auto;
